b779e83ea58c539c0b50fbdf485d336c45677192
sunwooiljoon <wnsdl1659@naver.com>
2023-04-12 21:43:29 +0900

    cpp03 ex00 done

---
+++ cpp/cpp_module_03/ex00/main.cpp
@@ -0,0 +1,22 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap A("aname");
+	ClapTrap B("bname");
+
+	A.attack("bname");
+	B.takeDamage(0);
+
+	B.takeDamage(5);
+	B.beRepaired(15);
+	B.takeDamage(25);
+
+	B.beRepaired(10); // hitpoints : 0
+
+	for (int i = 0; i < 9; i++)
+		A.attack("bname");
+
+	A.beRepaired(10);
+	A.attack("bname");
+}
