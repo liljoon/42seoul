a5940ea1d8dcd2a90a8eca117b873e3eaab676d9
sunwooiljoon <wnsdl1659@naver.com>
2023-05-07 13:25:26 +0900

    done ex03

--- cpp/cpp_module_04/ex03/main.cpp
+++ cpp/cpp_module_04/ex03/main.cpp
@@ -8,40 +8,40 @@
 
 int main()
 {
-	// IMateriaSource *src = new MateriaSource();
-	// src->learnMateria(new Ice());
-	// src->learnMateria(new Cure());
-	// ICharacter *me = new Character("me");
-	// AMateria *tmp;
-	// tmp = src->createMateria("ice");
-	// me->equip(tmp);
-	// tmp = src->createMateria("cure");
-	// me->equip(tmp);
-	// ICharacter *bob = new Character("bob");
-	// me->use(0, *bob);
-	// me->use(1, *bob);
-	// delete bob;
-	// delete me;
-	// delete src;
-	// return 0;
-
 	IMateriaSource *src = new MateriaSource();
-	Ice *a = new Ice();
-	Ice *b = new Ice(*a);
-	src->learnMateria(b);
+	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 	ICharacter *me = new Character("me");
 	AMateria *tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
-
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
 	ICharacter *bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
-	me->use(2, *bob);
-	me->use(4, *bob);
 	delete bob;
 	delete me;
 	delete src;
 	return 0;
+
+	// IMateriaSource *src = new MateriaSource();
+	// Ice *a = new Ice();
+	// Ice *b = new Ice(*a);
+	// src->learnMateria(b);
+	// src->learnMateria(new Cure());
+	// ICharacter *me = new Character("me");
+	// AMateria *tmp;
+	// tmp = src->createMateria("ice");
+	// me->equip(tmp);
+
+	// ICharacter *bob = new Character("bob");
+	// me->use(0, *bob);
+	// me->use(1, *bob);
+	// me->use(2, *bob);
+	// me->use(4, *bob);
+	// delete bob;
+	// delete me;
+	// delete src;
+	// return 0;
 }
