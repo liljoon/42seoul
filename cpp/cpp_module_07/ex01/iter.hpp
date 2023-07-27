3ff9e20bee213c5b176980c282fa1c6942547340
Iljoon Sunwoo <isunwoo@c3r3s7.42seoul.kr>
2023-07-27 21:22:55 +0900

    fix cpp07

--- cpp/cpp_module_07/ex01/iter.hpp
+++ cpp/cpp_module_07/ex01/iter.hpp
@@ -2,13 +2,6 @@
 #define ITER_HPP
 
 template <typename T>
-void iter(T *arr, int len, void (*func)(T))
-{
-	for (int i = 0; i < len; i++)
-		func(arr[i]);
-}
-
-template <typename T>
 void iter(T *arr, int len, void (*func)(T const &))
 {
 	for (int i = 0; i < len; i++)
