0742238d4978436b4efccf6788cb3fb558e40785
sunwooiljoon <wnsdl1659@naver.com>
2023-07-27 18:04:35 +0900

    done cpp07

--- cpp/cpp_module_07/ex02/main.cpp
+++ cpp/cpp_module_07/ex02/main.cpp
@@ -1,5 +1,30 @@
 #include "Array.hpp"
 
+#include <iostream>
+
 int main()
 {
+	{
+		Array<int> arr(10);
+
+		for (int i = 0; i < 10; i++)
+			std::cout << arr[i] << std::endl;
+		for (int i = 0; i < 10; i++)
+			arr[i] = i;
+		for (int i = 0; i < 10; i++)
+			std::cout << arr[i] << std::endl;
+	}
+	{
+		Array<int> arr;
+
+		try
+		{
+			std::cout << arr[11] << std::endl;
+		}
+		catch (const std::out_of_range &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	// system("leaks a.out");
 }
