9ff8bb88755eae1fd10e6f5a5f95f2d2fd28a010
Iljoon Sunwoo <isunwoo@c3r3s7.42seoul.kr>
2023-08-04 18:20:23 +0900

    cpp09 ex02 template recursion

--- cpp/cpp_module_09/ex02/PmergeMe.hpp
+++ cpp/cpp_module_09/ex02/PmergeMe.hpp
@@ -4,8 +4,9 @@
 #include <utility>
 #include <iostream>
 #include <vector>
+#include <typeinfo>
 
-template <typename T>
+template <typename T, int N>
 void PmergeMe(std::vector<T> &arr)
 {
 	if (arr.size() <= 1)
@@ -22,17 +23,30 @@ void PmergeMe(std::vector<T> &arr)
 			std::swap(temp[i].first, temp[i].second);
 	}
 
-	std::sort(temp.begin(), temp.end()); // 추후 recursive 로 변환
+	//std::sort(temp.begin(), temp.end());
+	PmergeMe<std::pair<T, T>, N - 1 >(temp); // 추후 recursive 로 변환
 
 	// for (size_t i = 0; i < temp.size(); i++)
 	// {
 	// 	std::cout<< temp[i].first << " "<<temp[i].second << std::endl;
 	// }
 	std::vector<T> sorted_temp;
-	for(size_t i = 0;i < sorted_temp.size(); i++)
+	for(size_t i = 0;i < temp.size(); i++) // 하나씩 넣고 하나씩 앞에 추가.
+	{
 		sorted_temp.push_back(temp[i].first);
 
-
+		typename std::vector<T>::iterator it_toinsert;
+		it_toinsert = std::lower_bound(sorted_temp.begin(), sorted_temp.end(), temp[i].second);
+		sorted_temp.insert(it_toinsert, temp[i].second);
+	}
+	arr = sorted_temp;
 }
+
+// template <typename T, 0>
+// void PmergeMe(T &arr)
+// {
+// 	(void)arr;
+// 	return ;
+// }
 
 #endif
--- cpp/cpp_module_09/ex02/main.cpp
+++ cpp/cpp_module_09/ex02/main.cpp
@@ -4,10 +4,41 @@
 int main()
 {
 	std::vector<int> v;
+
 	v.push_back(2);
 	v.push_back(4);
 	v.push_back(1);
 	v.push_back(3);
+	// v.push_back(7);
+	// v.push_back(10);
+	// v.push_back(9);
+	// v.push_back(8);
 
-	PmergeMe(v);
+	PmergeMe<int, 10>(v);
+
+	for(size_t i = 0;i < v.size();i++)
+		std::cout << v[i] << std::endl;
 }
+
+// #include <iostream>
+
+// // 템플릿 재귀를 이용한 팩토리얼 계산 함수
+// template <int N>
+// struct Factorial
+// {
+// 	static const int value = N * Factorial<N - 1>::value;
+// };
+
+// // 템플릿 재귀의 기저조건(Basis case)
+// template <>
+// struct Factorial<0>
+// {
+// 	static const int value = 1;
+// };
+
+// int main()
+// {
+// 	const int result = Factorial<5>::value;
+// 	std::cout << "Factorial of 5 is: " << result << std::endl;
+// 	return 0;
+// }
